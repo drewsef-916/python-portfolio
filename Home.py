7e5131490f176b435f4dbdc592515f4954bdd7fa
drewsef-916 <drewm541@gmail.com>
2024-01-03 13:18:03 -0800

    update link to todo app

--- Home.py
+++ Home.py
@@ -6,16 +6,16 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.jpeg")
 
 with col2:
     st.title("Andrew Moore")
     content = """
-    I am a software engineer from California. I like cats and guitars.
+    I am a software engineer from California. I specialize in writing code using React, TypeScript, and Python.
     """
     st.info(content)
 
-st.write("Below you can find some of the apps I have built in Python. This text should not be bound to the width of one column.")
+st.write("Below you can find some of the apps I have built in Python.")
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
